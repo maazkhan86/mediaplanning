b4fba44abf630fdac35eb2f40b6aedebc20fe1a3
Maaz A. Khan <maazkhan700@gmail.com>
2020-08-30 00:04:36 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='mediaplanning',
+  version='0.0.4',
+  description='Python package for media planning',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='https://github.com/maazkhan86/mediaplanning',  
+  author='Maaz A. Khan',
+  author_email='maazkhan700@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='media planning digital online offline', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
